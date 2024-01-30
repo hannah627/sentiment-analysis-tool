5dbcf46fce1366df4a22d178e9674031dfde144d
Hannah <hannah06@uw.edu>
2024-01-29 16:37:27 -0800

    can use multiple stopwords lists now
    also created custom stopwords list and used that on texts; also expanded lexicon

--- main.py
+++ main.py
@@ -2,17 +2,21 @@ from lexicon_scripts import checkLexiconForDuplicates
 from text_scripts import formatAllFilesInDirectory
 from text_scripts import applyStopWordsToDirectory
 from lexicon_scripts import scoreWords
+# from text_scripts import getStopWordsList
 
 
 corpus_1800s_dir = './corpus/1800s/'
 
+# sw_list = getStopWordsList()
+# print(sw_list)
+
 
 
 #checkLexiconForDuplicates()
-scoreWords("unnecessary gave none  second edition demands words acknowledgment miscellaneous remark thanks due three quarters public indulgent ear inclined plain tale pretensions press fair field honest suffrage opened obscure aspirant publishers  aid tact energy practical sense frank liberality afforded unknown unrecommended author press public vague personifications must thank vague terms  publishers definite ¹ messrs smith elder co gracious treatment publishers accorded charlotte brontë reader referred mrs gaskells admirable  life  gifted authoress ts  certain generous critics encouraged large hearted high minded men know encourage struggling stranger  ie publishers select reviewers say cordially gentlemen thank heart thus acknowledged owe aided approved turn another class  small one far know not therefore overlooked mean timorous carping doubt tendency books jane eyre whose eyes whatever unusual wrong")
+#scoreWords("unnecessary gave none  second edition demands words acknowledgment miscellaneous remark thanks due three quarters public indulgent ear inclined plain tale pretensions press fair field honest suffrage opened obscure aspirant publishers  aid tact energy practical sense frank liberality afforded unknown unrecommended author press public vague personifications must thank vague terms  publishers definite ¹ messrs smith elder co gracious treatment publishers accorded charlotte brontë reader referred mrs gaskells admirable  life  gifted authoress ts  certain generous critics encouraged large hearted high minded men know encourage struggling stranger  ie publishers select reviewers say cordially gentlemen thank heart thus acknowledged owe aided approved turn another class  small one far know not therefore overlooked mean timorous carping doubt tendency books jane eyre whose eyes whatever unusual wrong")
 
-# formatAllFilesInDirectory(corpus_1800s_dir)
-# applyStopWordsToDirectory(corpus_1800s_dir + 'formatted_files/')
+formatAllFilesInDirectory(corpus_1800s_dir)
+applyStopWordsToDirectory(corpus_1800s_dir + 'formatted_files/')
 
 # terms = getStopWordsList()
 # print(terms)
--- text_scripts.py
+++ text_scripts.py
@@ -11,15 +11,6 @@ def makeChangesToFileAndSaveToAnotherFile(fileToFormat, resultsFile, function):
             f2.write(results)
 
 
-# def formatAllFilesInDirectory(directory):
-#     files = getFilesFromDirectory(directory)
-#     full_file_names = [directory + k  for k in files]
-#     result_file_names = [directory + 'formatted_files/formatted_' + k for k in files]
-#     for i in range(len(files)):
-#         formatTextFile(full_file_names[i], result_file_names[i])
-#     print("Formatted all files in " + directory)
-
-
 def applyFunctionToAllFilesInDirectory(startingDirectory, resultsDirectory, function):
     files = getFilesFromDirectory(startingDirectory)
     full_file_names = [startingDirectory + k  for k in files]
@@ -27,11 +18,11 @@ def applyFunctionToAllFilesInDirectory(startingDirectory, resultsDirectory, func
     for i in range(len(files)):
         function(full_file_names[i], result_file_names[i])
 
+
 def getFilesFromDirectory(directory):
     files_and_directories = os.listdir(directory)
     files = [k for k in files_and_directories if k.endswith('.txt')]
     return files
-
 
 
 def formatAllFilesInDirectory(directory):
@@ -50,17 +41,25 @@ def lowerCaseAndRemovePunctuationOfText(text):
     return lowercase_text_sans_punctuation
 
 
-
 def getStopWordsList():
-    with open('./stop_words_list.txt') as f:
-        contents = f.read()
-        terms = contents.split('\n')
-    return terms
+# compiles stopwords list from all text files in the stop_words_lists directory; returns single list with all terms
+    complete_sw_list = []
+    stopwords_lists = getFilesFromDirectory('./stop_words_lists/')
+    for sw_list in stopwords_lists:
+        with open('./stop_words_lists/' + sw_list) as f:
+            contents = f.read()
+            terms = contents.split('\n')
+            for term in terms:
+                complete_sw_list.append(term)
+    return complete_sw_list
 
 
 def applyStopWordsToDirectory(directory):
+    # parentDirectory = "." + directory
+    # print(parentDirectory) # ../corpus/1800s/formatted_files
     resultsDirectory = directory + 'formatted_files_without_stopwords/'
     applyFunctionToAllFilesInDirectory(directory, resultsDirectory, applyStopWords)
+    print("Applied stopwords list to all files in " +  directory)
 
 
 def applyStopWords(fileToFilter, resultsFile):
@@ -70,9 +69,12 @@ def applyStopWords(fileToFilter, resultsFile):
 
 def filteringWords(text):
     stop_words = getStopWordsList()
+
+    # need to do this because sometimes there are newline chars in the middle of two words instead of a space
     tokens_with_newline_chars = text.split(' ')
     rejoined_tokens = '\n'.join(tokens_with_newline_chars)
     clean_tokens = rejoined_tokens.split('\n')
+
     filtered_words = [word for word in clean_tokens if word not in stop_words]
     results = ' '.join(filtered_words)
     return results
