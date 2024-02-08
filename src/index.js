a6f230d54c91e6d6d0c120a731ade61e4f5cc7c3
Hannah <hannah06@uw.edu>
2024-02-08 11:33:04 -0800

    created options section and lexicon options; added file input

--- src/index.js
+++ src/index.js
@@ -2,29 +2,68 @@
 
 (function() {
 
+const AFINN_LEXICON = "../lexicons/afinn-lexicon-en-165.txt";
+const HISTORICAL_LEXICON = "../lexicons/lexicon-v1";
+
 // when the page loads, call "init"
 window.addEventListener("load", init);
 
 
 // the function that runs when the page loads;
 function init() {
-    id("input").addEventListener("input", processInputtedText);
+    // hiding things that shouldn't be visible when the page loads
     id("optionsSection").style.display = "none";
-    id("toggleOptionsSectionBtn").addEventListener("click", toggleOptionsSection)
+    id("customLexiconSection").style.display = "none";
+
+    // adding general event listeners (options section button, main text input)
+    id("toggleOptionsSectionBtn").addEventListener("click", toggleOptionsSectionVisibility);
+    id("input").addEventListener("input", processInputtedText);
+    id("inputfile").addEventListener("change", processInputFile);
+
+    // finding lexicon option checkboxes and adding event listeners to them
+    let lexiconOptions = document.querySelectorAll('input[name=lexicon]');
+    lexiconOptions.forEach((e) => e.addEventListener("change", findLexicon));
+    id("lexicon3").addEventListener("change", toggleCustomLexiconAreaVisibility);
+
 }
 
-function toggleOptionsSection() {
+
+function findLexicon() {
+    let checkedBoxes = document.querySelectorAll('input[name=lexicon]:checked');
+    let currentLexicons = [];
+    checkedBoxes.forEach((e) => currentLexicons.push(e.value));
+
+    console.log(currentLexicons);
+    console.log(AFINN_LEXICON)
+
+    for (let i = 0; i < currentLexicons.length; i++) {
+        // fetch the file for the current lexicon
+    }
+
+
+}
+
+function toggleCustomLexiconAreaVisibility() {
+    let customLexiconSection = id("customLexiconSection");
+    if (customLexiconSection.style.display === "none") {
+        customLexiconSection.style.display = "flex";
+    } else {
+        customLexiconSection.style.display = "none"
+    }
+
+}
+
+function toggleOptionsSectionVisibility() {
     let optionsSection = id("optionsSection")
     let arrowButton = id("arrowBtn");
-    // if (optionsSection.classList.contains(''))
     if (optionsSection.style.display === "none") {
-        optionsSection.style.display = "block";
+        optionsSection.style.display = "flex";
         arrowButton.classList.remove('down');
-        arrowButton.classList.add('up')
+        arrowButton.classList.add('up');
     } else {
         optionsSection.style.display = "none";
         arrowButton.classList.remove('up');
-        arrowButton.classList.add('down')
+        arrowButton.classList.add('down');
     }
 }
 
@@ -34,8 +73,17 @@ function toggleOptionsSection() {
 
 function processInputtedText() {
     let inputtedText = id("input").value;
-    let formatted_text = lowerCaseAndRemovePunctuationOfText(inputtedText)
+    let formatted_text = lowerCaseAndRemovePunctuationOfText(inputtedText);
     id("result").textContent = formatted_text;
+}
+
+function processInputFile() {
+    let fr = new FileReader();
+    fr.onload = function() {
+        let formatted_text = lowerCaseAndRemovePunctuationOfText(fr.result)
+        id("result").textContent = formatted_text;
+    }
+    fr.readAsText(this.files[0]);
 }
 
 function lowerCaseAndRemovePunctuationOfText(text) {
@@ -45,9 +93,21 @@ function lowerCaseAndRemovePunctuationOfText(text) {
     return textWithoutExtraSpaces;
 }
 
+
+
+
+
+
 function removeStopWords(text) {
 
 }
+
+
+
+
+
+
+
 
 
 /**
